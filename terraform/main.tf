1a1a35d75405210cf558b984d41e7b7442497fa1
Ashwin52 <ashwin2005aks@gmail.com>
2026-05-22 13:40:27 +0000

    Day 2: Terraform ECR + ECS cluster, Docker image pushed to ECR

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# ECR Repository
+resource "aws_ecr_repository" "app" {
+  name                 = "deployment-platform-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# ECS Cluster
+resource "aws_ecs_cluster" "main" {
+  name = "deployment-platform-cluster"
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
